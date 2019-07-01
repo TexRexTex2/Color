04eab589617c9a6a8b973e00229491c4b333e42d
TexRexTex2 <52430158+TexRexTex2@users.noreply.github.com>
2019-07-02 10:36:16 +1200

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ const urlParams = new URLSearchParams(window.location.search);
 const color = urlParams.get('color');
 document.body.style.backgroundColor = color;
 document.title=color;
+console.log(color);
